e04b594daf9ad91faf6a2fd52b762503b78384b7
Johan Wennerbeck <Jojjanw@gmail.com>
2017-10-17 15:01:28 +0200

    Change speedvalues in acc

--- canJava/JavaInterfaceTest/ACC/absolut/acc/ACC.java
+++ canJava/JavaInterfaceTest/ACC/absolut/acc/ACC.java
@@ -27,7 +27,7 @@ public class ACC implements Runnable {
     //Bad values: 7 and 27
     //int[] speedValues = new int[]{0, 7, 11, 15, 19, 23, 27, 37, 41, 45, 49, 53, 57, 73, 77, 85, 89, 93, 97, 100};
     //int[] speedValues = new int[]{0, 9, 11, 13, 15, 17 ,19, 21};
-    int[] speedValues = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int[] speedValues = new int[]{0,9,10,11,12,13,14,15,16,17,18,19,20};
     int currentSpeed;
 
     //En funktion som raknar ut ultimata distance, utbyte mot perfdist konstanten.
