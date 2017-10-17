ab61d16fd60b3a580d35ef448981e2235ceb50e0
Peter Gärdenäs <gardenas@student.chalmers.se>
2017-10-17 17:14:06 +0200

    Made build take an argument as well

--- canJava/JavaInterfaceTest/ACC/build.sh
+++ canJava/JavaInterfaceTest/ACC/build.sh
@@ -3,7 +3,7 @@
 javac -cp "jna-4.5.0.jar:." -sourcepath . absolut/can/*.java
 javac -cp "jna-4.5.0.jar:." -sourcepath . absolut/acc/*.java
 
-./run.sh
+./run.sh $1
 #if ! pgrep -x "rmiregistry" > /dev/null
 #then
 #    rmiregistry &
