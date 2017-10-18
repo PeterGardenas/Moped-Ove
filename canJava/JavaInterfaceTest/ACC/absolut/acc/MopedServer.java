9e2a79a8db6d5869fabb434730472daa5e52ce80
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-10-18 09:17:11 +0200

    Hanna and Elina cleaned up the code and added comments

--- canJava/JavaInterfaceTest/ACC/absolut/acc/MopedServer.java
+++ canJava/JavaInterfaceTest/ACC/absolut/acc/MopedServer.java
@@ -1,9 +1,8 @@
 package absolut.acc;
 
-import java.io.*;
-
 import absolut.can.CanReader;
 
+import java.io.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
@@ -12,8 +11,6 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class MopedServer {
-
-
 
 	public static void init() {
 		//Copied from some stack overflow thread.
@@ -31,20 +28,20 @@ public class MopedServer {
 	//Receives a post request, handles it and sends a response. 
 	static class ImageHandler implements HttpHandler {
 
+		//Saves the previous deviation of the red dot from image center
 		private double prevOffset;
 
+		//If a red dot is detected, the method steers the car
 		public void handle(HttpExchange t) throws IOException {
 	        t.sendResponseHeaders(200, 1);
-	        String message = getMessage(t.getRequestBody());
+	        String message = getMessage(t.getRequestBody()); //Gets the message from the camera, false if no red circle was found, and a number ((-100) - 100) if red circle is found
 	        t.close();
 	        if (!message.equals("false")) {
 	        	double steerValue = 0;
-	        	double lowPercentage = 0.4;
-	        	double highPercentage = 0.8;
-	        	double offset = Double.parseDouble(message);
-	        	if (((offset >= 0 && prevOffset >= 0) || (offset < 0 && prevOffset < 0)) && Math.abs(offset - prevOffset) > 50) {
-	        		steerValue = offset * highPercentage;
-				} else if (((offset >= 0 && prevOffset < 0) || (offset < 0 && prevOffset >= 0)) && ((Math.abs(offset) + Math.abs(prevOffset)) > 50)) {
+	        	double lowPercentage = 0.4; // Percentage used to steer the car in small turns
+	        	double highPercentage = 0.8; // Percentage used to steer the car in sharp turns
+	        	double offset = Double.parseDouble(message); //The deviation of the red dot from image center
+	        	if (isSharpTurn(offset)) {
 	        		steerValue = offset * highPercentage;
 				} else {
 	        		steerValue = offset * lowPercentage;
@@ -52,20 +49,29 @@ public class MopedServer {
 				int steerValueTmp = (int) Math.floor(steerValue);
 	        	try {
 	        		prevOffset = offset;
-					CanReader.getInstance().sendSteering((byte) steerValueTmp);
+					CanReader.getInstance().sendSteering((byte) steerValueTmp); //Sends steer value to the can bus
 				} catch (Exception e) {
 	        		e.printStackTrace();
 				}
 			}
 	    }
+
+	    //Returns true when the turn is sharp
+	    private boolean isSharpTurn(double offset) {
+			int limitForSharpTurn = 50;
+			return ((((offset >= 0 && prevOffset >= 0) || (offset < 0 && prevOffset < 0))
+					&& Math.abs(offset - prevOffset) > limitForSharpTurn) || (((offset >= 0 && prevOffset < 0) ||
+					(offset < 0 && prevOffset >= 0)) &&
+					((Math.abs(offset) + Math.abs(prevOffset)) > limitForSharpTurn)));
+		}
 	}
 
+	//Receives an input stream, detects the message from the input stream and returns it
 	public static String getMessage(InputStream is) {
 		BufferedReader in = new BufferedReader(
 				new InputStreamReader(is));
 		String inputLine;
 		StringBuffer response = new StringBuffer();
-
 		try {
 			while ((inputLine = in.readLine()) != null) {
 				response.append(inputLine);
@@ -74,9 +80,7 @@ public class MopedServer {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		String temp = response.toString();
 		return temp.substring(temp.indexOf("startM") + "startM".length(), temp.indexOf("endM"));
-
 	}
 }
